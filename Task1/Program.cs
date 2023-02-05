1734f8bacd3692f7d184705f40d46dbc5df59e40
Daria <darya.shumilova.03@bk.ru>
2023-02-05 14:01:43 +0600

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+class Palindrom
+{
+    static bool programm(int n)
+    {
+        int b = 0, nn = n;
+        while (n > 0)
+        {
+            b = b * 10 + n % 10;
+            n = n / 10;
+        }
+        return b == nn;
+    }
+    static void Main()
+    {
+        int a;
+        Console.Write("Введите любое число: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        if (programm(a))
+            Console.WriteLine("Палиндром");
+        else
+            Console.WriteLine("Не палиндром");
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+System.Console.Write("Введите координаты Ax точки: ");
+double Ax = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координаты Ay точки: ");
+double Ay = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координаты Cx точки: ");
+double Cx = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координаты Bx точки: ");
+double Bx = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координаты By точки: ");
+double By = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координаты Cy точки: ");
+double Cy = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay, 2) + Math.Pow(Cx - Cy, 2));
+System.Console.WriteLine(distance);
